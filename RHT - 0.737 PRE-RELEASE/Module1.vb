5b594ac5d562f0cc85dc67b1d6817fcbcbebd2eb
RedX <82812868+PixcellSTD@users.noreply.github.com>
2024-02-13 22:26:00 +0300

    Add files via upload

---
+++ RHT - 0.737 PRE-RELEASE/Module1.vb
@@ -0,0 +1,52 @@
+﻿Module Module1
+    Sub Main()
+        ' Feel free to use this.
+        ' Set the background color of the console to black
+        Console.BackgroundColor = ConsoleColor.Black
+
+        ' Set the text color to white for better visibility
+        Console.ForegroundColor = ConsoleColor.White
+
+        ' Get the command from the user
+        Do
+            ' Process the command
+            Dim output As String = ProcessCommand()
+
+            ' Display the output only if it's not empty
+            If Not String.IsNullOrEmpty(output) Then
+                ' Display the command and output
+                Console.WriteLine("/" & output)
+            Else
+                ' Display a message for commands with no specific output
+                Console.WriteLine("No specific output for this command.")
+            End If
+
+            ' Reset the console colors
+            Console.ResetColor()
+
+        Loop While True
+    End Sub
+
+    Private Function ProcessCommand() As String
+        ' Customize this function to implement your command processing logic
+        Dim command As String = Console.ReadLine()
+
+        Dim tokens As String() = command.Split(" "c)
+        ' Get the command from the user
+
+        Select Case tokens(0).ToLower()
+            Case "hello"
+                Return "Hello!"
+            Case "time"
+                Return "Current time: " & DateTime.Now.ToString("HH:mm:ss")
+            Case "about"
+                Return "0.737 pre-release - Developed by redx"
+            Case "write"
+                ' Join the arguments (excluding the "print" part) and return as the output
+                Return String.Join(" ", tokens.Skip(1))
+            Case Else
+                Return String.Empty ' Return an empty string for unknown commands
+
+        End Select
+    End Function
+End Module
---
+++ RHT - 0.737 PRE-RELEASE/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("RHT - 0.737 PRE-RELEASE")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("RHT - 0.737 PRE-RELEASE")> 
+<Assembly: AssemblyCopyright("Copyright ©  2024")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8605dd7f-1fa7-4834-8f02-7a18092b3df2")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
